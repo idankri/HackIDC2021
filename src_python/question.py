fe23af2d774124fa45b10565d4296bac88f33403
Guy Schneider <guysch4@gmail.com>
2021-06-03 16:57:40 +0300

    Added questions and items

---
+++ src_python/question.py
@@ -0,0 +1,26 @@
+class Question:
+    def __init__(self, type_of_item, message_to_user, invalid_message, answer_validation_function, get_next_question_id,
+                 answer_flag, image_flag):
+        self.type = type_of_item
+        self.message_to_user = message_to_user
+        self.invalid_message = invalid_message
+        self.answer_validation_function = answer_validation_function
+        self.get_next_question_id = get_next_question_id
+        self.answer_flag = answer_flag
+        self.image_flag = image_flag
+
+    def process_answer(self, user_answer : str, user_data) -> int:
+        if self.answer_validation_function():
+            self.update_user_data(user_answer, user_data)
+            return self.get_next_question_id(user_answer)
+        return -1
+
+    #def process_image(self, user_image):
+    def get_message(self) -> str:
+        return self.message_to_user
+
+    def get_invalid_message(self) -> str:
+        return self.invalid_message
+
+    def update_user_data(self, user_answer : str, user_data) -> None:
+        user_data.set(self.type, user_answer)
