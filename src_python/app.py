9c85fd8df1a40b2bddd93bd49a642be29ed722b5
idankri <58448747+idankri@users.noreply.github.com>
2021-06-28 15:45:13 +0300

    Post hackathon code cleanup

--- src_python/app.py
+++ src_python/app.py
@@ -6,9 +6,9 @@ from conversation_manager import main_conversation_manager
 
 app = Flask(__name__)
 
-TWILIO_NUMBER = "+14155238886"
-account_sid = 'AC6bb703567bab46b4b8c2dff452892fb5'
-auth_token = '86add94c5884e3dcef8298494dca251b'
+TWILIO_NUMBER = "" # Should be Twilio number
+account_sid = '' # Should be Twilio account SID
+auth_token = '' # Should be Twilio Auth token
 
 
 @app.route("/bot", methods=['POST'])
@@ -31,38 +31,12 @@ def sendMessage(text, number):
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
-        from_='whatsapp:+14155238886',
+        from_=f'whatsapp:{TWILIO_NUMBER}',
         body=text,
         to=f'whatsapp:{number}'
     )
     print(message.sid)
 
-
-'''
-@app.route("/bot", methods=['POST'])
-def bot():
-    incoming_msg = request.values.get('Body', '').lower()
-    resp = MessagingResponse()
-    msg = resp.message()
-    responded = False
-    if 'quote' in incoming_msg:
-        # return a quote
-        r = requests.get('https://api.quotable.io/random')
-        if r.status_code == 200:
-            data = r.json()
-            quote = f'{data["content"]} ({data["author"]})'
-        else:
-            quote = 'I could not retrieve a quote at this time, sorry.'
-        msg.body(quote)
-        responded = True
-    if 'cat' in incoming_msg:
-        # return a cat pic
-        msg.media('https://cataas.com/cat')
-        responded = True
-    if not responded:
-        msg.body('I only know about famous quotes and cats, sorry!')
-    return str(resp)
-'''
 
 if __name__ == '__main__':
     app.run()
